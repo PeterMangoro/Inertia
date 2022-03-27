e623b78161e9a734c970dd3daf402e96684141ca
PeterMangoro <48471525+PeterMangoro@users.noreply.github.com>
2022-03-27 11:05:07 +0200

    test

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <app-layout title="Dashboard">
+    
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
